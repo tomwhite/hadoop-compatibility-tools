b5c3854385f63fa12d566687300c6cb1ebbfa8e2
Tom White <tom@cloudera.com>
2011-10-12 14:08:14 -0700

    Add check for 0.20.204.0 to 0.24.0-SNAPSHOT (HADOOP-7738)

---
+++ apicheck-hadoop-0.20.204.0-0.24.0-SNAPSHOT.sh
@@ -0,0 +1,21 @@
+BASELINE_VERSION=0.20.204.0
+NEW_VERSION=0.24.0-SNAPSHOT
+
+cat hadoop_private_elements hadoop_hidden_classes > excludes
+
+./apicheck.sh \
+  --project=hadoop \
+  --package=org.apache.hadoop \
+  --baseline-version=$BASELINE_VERSION \
+  --baseline-tar=http://archive.apache.org/dist/hadoop/core/hadoop-$BASELINE_VERSION/hadoop-${BASELINE_VERSION}.tar.gz \
+  --baseline-dir=hadoop-${BASELINE_VERSION} \
+  --baseline-classpath-includes='{hadoop-core-$BASELINE_VERSION.jar,lib/*.jar,lib/jsp-2.1/*.jar}' \
+  --baseline-classpath-excludes='lib/hadoop-fairscheduler-$BASELINE_VERSION.jar' \
+  --new-version=$NEW_VERSION \
+  --new-tar=file:///Users/tom/workspace/hadoop-trunk/hadoop-dist/target/hadoop-$NEW_VERSION.tar.gz \
+  --new-classpath-includes='{share/hadoop/common/hadoop-common-$NEW_VERSION.jar,share/hadoop/hdfs/hadoop-hdfs-$NEW_VERSION.jar,share/hadoop/{common,hdfs}/lib/*.jar,lib/*.jar,modules/*.jar}' \
+  --new-classpath-excludes='lib/hadoop-fairscheduler-$NEW_VERSION.jar' \
+  --excluded-annotations='org.apache.hadoop.classification.InterfaceAudience$Private,org.apache.hadoop.classification.InterfaceAudience$LimitedPrivate,org.apache.hadoop.classification.InterfaceStability$Unstable' \
+  --excludes-file=excludes \
+  --hadoop-annotation-tools-jar=lib/* \
+  --java-tools-classpath=$JAVA_HOME/../Classes/classes.jar:$JAVA_HOME/../Classes/jsse.jar:$JAVA_HOME/../Classes/jce.jar:$ANT_HOME/lib/ant.jar
